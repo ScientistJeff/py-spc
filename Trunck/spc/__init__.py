b4c780b6274ce983c5a4d302d19f42965198882c
c.giacofei@gmail.com <c.giacofei@gmail.com@2d68e65e-76f1-1cee-679b-a02fde6db724>
2011-08-23 21:34:02 +0000

    Delete

--- Trunck/spc/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-SPC Statistical Process Control provides means to monitor process behavior
-using statistical tools defined by Shewhart and others. The process run is shown
-as Quality Control Charts (QCC).
-
-Author: Chris Giacofei <c.giacofei@gmail.com>
-
-License: GNU
-"""
