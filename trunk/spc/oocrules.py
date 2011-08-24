a911dd9042c24f90d7a8a379f75a2f3f5bf4df4d
C.Giacofei@gmail.com <C.Giacofei@gmail.com@2d68e65e-76f1-1cee-679b-a02fde6db724>
2011-08-24 19:21:59 +0000

    Added functions: test_beyond_1sigma test_beyond_2sigma test_trending_runs
    changes not yet tested

--- trunk/spc/oocrules.py
+++ trunk/spc/oocrules.py
@@ -1,10 +1,68 @@
-
+"""
+Contains functions that will identify OOC
+points in a set of data
+"""
 
 def test_beyond_limits(data, center, lcl, ucl):
+"""Checks for any point outside control limits"""
     return data[0] > ucl or data[0] < lcl
 
 def test_violating_runs(data, center, lcl, ucl):
+"""Test for all points above or below center"""
+
     for i in range(1, len(data)):
         if (data[i-1] - center)*(data[i] - center) < 0:
             return False
     return True
+
+def test_beyond_1sigma(data, center, lcl, ucl):
+    count_above = 0
+    count_below = 0
+    
+    upper = center + (ucl - lcl) / 6
+    lower = center - (ucl - lcl) / 6
+    
+    if data[i] > upper:
+        count_above += 1
+    if data[i] < lower
+        count_below += 1
+        
+    if count_above >= len(data) or count_below <= len(data)
+        return True
+    return False
+    
+def test_beyond_2sigma(data, center, lcl, ucl):
+    count_above = 0
+    count_below = 0
+    
+    upper = center + (ucl - lcl) / 3
+    lower = center - (ucl - lcl) / 3
+    
+    if data[i] > upper:
+        count_above += 1
+    if data[i] < lower
+        count_below += 1
+        
+    if count_above >= len(data) or count_below <= len(data)
+        return True
+    return False
+    
+def test_trending_runs(data, center, lcl, ucl):
+    up = True
+    down = True
+    
+    for i in range(1, len(data)):
+        # Check if trending down
+        # Set false if any 2 trend down
+        if data[i-1] > data[i]:
+            up = False
+        # Check if trending up
+        # Set false if any 2 trend up
+        if data[i-1] < data[i]:
+            down = False
+        
+        # Return False if no trend, else true    
+        if up == False and down == False:
+            return False
+        else
+            return True
