74b37d3c5530d37f8d6fdd80c182e3037a560985
C.Giacofei@gmail.com <C.Giacofei@gmail.com@2d68e65e-76f1-1cee-679b-a02fde6db724>
2011-08-24 20:25:15 +0000

    Damn, last commit was screwed up.  This one actually works...

--- trunk/spc/oocrules.py
+++ trunk/spc/oocrules.py
@@ -4,56 +4,67 @@ points in a set of data
 """
 
 def test_beyond_limits(data, center, lcl, ucl):
-"""Checks for any point outside control limits"""
+    """Checks for any point outside control limits"""
     return data[0] > ucl or data[0] < lcl
 
 def test_violating_runs(data, center, lcl, ucl):
-"""Test for all points above or below center"""
+    """Test for all points above or below center"""
 
-    for i in range(1, len(data)):
-        if (data[i-1] - center)*(data[i] - center) < 0:
-            return False
-            
+    for idx, curr in enumerate(data):
+        if idx > 0:
+            if (prev - center)*(curr - center) < 0:
+                return False
+        prev = curr
+        
     return True
 
-def test_beyond_1sigma(data, center, lcl, ucl):
+def test_beyond_onesigma(data, center, lcl, ucl):
+    """Test for data - 1 points above or below 1 sigma"""
+
     count_above = 0
     count_below = 0
     
     upper = center + (ucl - lcl) / 6
     lower = center - (ucl - lcl) / 6
     
-    if data[i] > upper:
-        count_above += 1
-    if data[i] < lower
-        count_below += 1
-        
-    if count_above >= len(data) - 1 or count_below <= len(data) - 1:
+    for idx, curr in enumerate(data):
+        if curr > upper:
+            count_above += 1
+        if curr < lower:
+            count_below += 1
+   
+    if count_above >= idx or count_below >= idx:
         return True
         
     return False
     
-def test_beyond_2sigma(data, center, lcl, ucl):
+def test_beyond_twosigma(data, center, lcl, ucl):
+    """Test for data - 1 points above or below 2 sigma"""
+
     count_above = 0
     count_below = 0
     
     upper = center + (ucl - lcl) / 3
     lower = center - (ucl - lcl) / 3
     
-    if data[i] > upper:
-        count_above += 1
-    if data[i] < lower
-        count_below += 1
-        
-    if count_above >= len(data) - 1 or count_below <= len(data) - 1:
+    for idx, curr in enumerate(data):
+        if curr > upper:
+            count_above += 1
+        if curr < lower:
+            count_below += 1
+   
+    if count_above >= idx or count_below >= idx:
         return True
         
     return False
     
 def test_trending_runs(data, center, lcl, ucl):
+    """Test for point trending up or down"""
+    
     up = True
     down = True
-    
+
+    #ToDo: Change to enumerate
     for i in range(1, len(data)):
         # Check if trending down
         # Set false if any 2 trend down
